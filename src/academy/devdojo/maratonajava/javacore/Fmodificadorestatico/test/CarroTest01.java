aec41be29abbbe11903dd653a79e9236250e73fd
gustta11 <gustavokeven50@gmail.com>
2025-06-03 21:26:47 -0300

    Primeiro commit

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,8 +4,8 @@ import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
-        //Carro carro01 = new Carro("Mustang", 280);
-        //carro01.imprime();
+        Carro carro01 = new Carro("Mustang", 280);
+        carro01.imprime();
 
         System.out.println(Carro.velocidadeLimite);
     }
