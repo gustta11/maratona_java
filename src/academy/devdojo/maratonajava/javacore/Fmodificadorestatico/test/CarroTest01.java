6b50693d8891e2832ec1743ee87379a6d0768ad7
gustta11 <gustavokeven50@gmail.com>
2025-06-03 21:54:29 -0300

    Adicionando metodos estaticos

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,10 +4,11 @@ import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
-        Carro carro01 = new Carro("Mustang", 280);
-        carro01.imprime();
+        //Carro carro01 = new Carro("Mustang", 280);
+        //carro01.imprime();
 
-        System.out.println(Carro.velocidadeLimite);
+        Carro.setVelocidadeLimite(220);
+        System.out.println(Carro.getVelocidadeLimite());
     }
 
 
