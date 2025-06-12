cc80baa798044934263846c2be565815b012d712
gustta11 <gustavokeven50@gmail.com>
2025-06-12 16:43:51 -0300

    Adicionando Inputs do teclado

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class LeituraDoTecladoTest01 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("PREVISÃO DO FUTURO");
+        System.out.println("Faça sua pergunta e eu responderei");
+
+        String pergunta = input.nextLine();
+        if(pergunta.charAt(0) == ' '){
+            System.out.println("SIM");
+        }else{
+            System.out.println("NÃO");
+        }
+
+
     }
 }
